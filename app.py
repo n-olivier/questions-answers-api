4f222cc56e3ca29460b5501092b8424a642e125c
n-olivier <onshimiyimana17@alustudent.com>
2019-10-29 10:54:54 +0200

    another code test

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/<qId>')
+def display_question(qId):
+    return "The question is {}.".format(qId)
+
 if __name__ == '__main__':
     app.run()
