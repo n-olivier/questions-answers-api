0bec7d157c86174bdcd9624ecf368f4c5ccfd123
n-olivier <onshimiyimana17@alustudent.com>
2019-10-29 11:19:28 +0200

    set up configurations

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+
 @app.route('/<qId>')
 def display_question(qId):
     return "The question is {}.".format(qId)
 
+
 if __name__ == '__main__':
     app.run()
+
+print(os.environ['APP_SETTINGS'])
