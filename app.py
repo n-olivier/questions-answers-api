1503723620cb666a1d67541d8ae10db1d5129b12
n-olivier <onshimiyimana17@alustudent.com>
2019-10-30 15:15:34 +0200

    added database configurations

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLALCHEMY(app)
 
+from models import Question
 
 @app.route('/')
 def hello():
